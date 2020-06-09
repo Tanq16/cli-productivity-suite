427f0f5510c281e9edec8717575ee85ce759c574
Apurv Singh Gautam <apurvsinghgautam@gmail.com>
2020-06-09 17:15:06 -0400

    Updated with Powerlevel10k theme
    Fastest zsh theme

--- install_zsh.sh
+++ install_zsh.sh
@@ -4,10 +4,11 @@ sudo apt install zsh curl git
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
 cat ~/.zshrc | sed -e "s/plugins=/plugins=(git zsh-autosuggestions zsh-syntax-highlighting) #/" > temp_file_for_zsh_rc
 cat temp_file_for_zsh_rc > ~/.zshrc
 rm temp_file_for_zsh_rc
-cat ~/.zshrc | sed -e "s/robbyrussel/agnoster/" > temp_file_for_zsh_rc
+cat ~/.zshrc | sed -e "s/robbyrussel/powerlevel10k/powerlevel10k/" > temp_file_for_zsh_rc
 cat temp_file_for_zsh_rc > ~/.zshrc
 rm temp_file_for_zsh_rc
 
