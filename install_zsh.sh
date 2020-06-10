bb01c1d7e3499ff58e3a56c34e5f5fcc3fac722b
Apurv Singh Gautam <apurvsinghgautam@gmail.com>
2020-06-10 19:56:26 -0400

    Updated with Powerlevel10k option

--- install_zsh.sh
+++ install_zsh.sh
@@ -4,8 +4,17 @@ sudo apt install zsh curl git
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-sed -i "s/plugins=/plugins=(git zsh-autosuggestions zsh-syntax-highlighting) #/" ~/.zshrc
+
+echo ('Choose a theme: 1.Powerlevel10k 2.Agnoster ')
+read choice
+if [ $choice -eq 1 ]; then
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+sed -i "s/robbyrussel/powerlevel10k\/powerlevel10k/" ~/.zshrc
+else
 sed -i "s/robbyrussel/agnoster/" ~/.zshrc
+fi
+
+sed -i "s/plugins=/plugins=(git zsh-autosuggestions zsh-syntax-highlighting) #/" ~/.zshrc
 
 echo "alias c=clear" >> .zshrc
 echo "alias l='ls -l'" >> .zshrc
