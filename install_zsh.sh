6fb891cc2bf72c54b9bae5d37107e64317324974
Tanishq Rupaal <37408906+Tanq16@users.noreply.github.com>
2020-06-11 18:34:04 -0400

    change install option to default yes

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt install zsh curl git
+sudo apt install -y zsh curl git
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
