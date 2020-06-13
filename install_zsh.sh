5af6b7e5d8c0661be38da698749ad714b791a06c
Tanq16 <tanishqrupaal@yahoo.com>
2020-06-13 01:15:24 -0400

    Correct issues in script

--- install_zsh.sh
+++ install_zsh.sh
@@ -1,35 +1,31 @@
 #!/bin/sh
 
-sudo apt install -y zsh curl git
-curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh > install_hahanet.sh
-sh install_hahanet.sh --unattended
+sudo apt install -y zsh wget git
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+sh install.sh --unattended
 
-echo """______________________________________________________________________________
+echo "Setting ZSH to default shell :: Please enter your password."
+chsh -s /usr/bin/zsh $USER
 
-Choose an installation:
-1. OhMyZsh (Recommended, good for all)
-2. Powerlevel10k (Fast alternative, with more features that need installation. Good for heavy customization)
-______________________________________________________________________________
-"""
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k 2>/dev/null
+sed -i "s/robbyrussell/powerlevel10k\/powerlevel10k/" ~/.zshrc
 
-read choice
-if [ $choice -eq 2 ]
-then
-        git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
-        sed -i "s/robbyrussel/powerlevel10k\/powerlevel10k/" ~/.zshrc
-else
-        sed -i "s/robbyrussel/agnoster/" ~/.zshrc
-fi
+echo "Custom shell installed."
+echo "Installing Auto-suggestions"
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions 2>/dev/null
+echo "Installing Syntax highlighting"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting 2>/dev/null
 
 sed -i "s/plugins=/plugins=(git zsh-autosuggestions zsh-syntax-highlighting) #/" ~/.zshrc
+echo "alias c=clear" >> ~/.zshrc
+echo "alias l='ls -l'" >> ~/.zshrc
+echo "alias la='ls -la'" >> ~/.zshrc
 
-echo "alias c=clear" >> .zshrc
-echo "alias l='ls -l'" >> .zshrc
-echo "alias la='ls -la'" >> .zshrc
-
+echo "Installing Awesome color scheme"
 git clone https://github.com/seebi/dircolors-solarized.git dirco_for_script_color_option 2>/dev/null
 cp dirco_for_script_color_option/dircolors.256dark ~/.oh-my-zsh/dircolors.256dark
 rm -rf dirco_for_script_color_option/
-echo "eval \`dircolors ~/.oh-my-zsh/dircolors.256dark\`" >> .zshrc
+echo "eval \`dircolors ~/.oh-my-zsh/dircolors.256dark\`" >> ~/.zshrc
 
-source ~/.zshrc
+echo "If you chose Powerlevel10k installation, the installation options"
+exec zsh -l
