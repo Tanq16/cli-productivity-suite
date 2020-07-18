1a53ebeac4ddd1805b61b7e4c23313bf49fc5613
Tanq16 <tanishqrupaal@yahoo.com>
2020-07-17 20:21:17 -0400

    output redirects

--- vim_improve.sh
+++ vim_improve.sh
@@ -1,21 +1,26 @@
 #!/usr/bin/zsh
 echo "Installing vim"
-sudo apt install vim
+sudo apt install -y vim 1>/dev/null 2>/dev/null
 
-git clone https://github.com/itchyny/lightline.vim ~/.vim/pack/plugins/start/lightline
-git clone https://github.com/jiangmiao/auto-pairs.git ~/auto_pairs_vim
+echo "Installing Lightline"
+git clone https://github.com/itchyny/lightline.vim ~/.vim/pack/plugins/start/lightline 2>/dev/null
+echo "Installing Bracket Auto-Pair"
+git clone https://github.com/jiangmiao/auto-pairs.git ~/auto_pairs_vim 2>/dev/null
 mkdir ~/.vim/plugin
 cp ~/auto_pairs_vim/plugin/auto-pairs.vim ~/.vim/plugin/
 rm -rf ~/auto_pairs_vim
 
-wget https://www.vim.org/scripts/download_script.php\?src_id\=21752
+echo "Installing SuperTab"
+wget https://www.vim.org/scripts/download_script.php\?src_id\=21752 2>/dev/null
 mv 'download_script.php?src_id=21752' ~/.vim/supertab.vmb
 vim -c 'so %' -c 'q' ~/.vim/supertab.vmb
 
-wget https://raw.githubusercontent.com/dylnmc/novum.vim/master/colors/novum.vim
+echo "Installing novum colorscheme"
+wget https://raw.githubusercontent.com/dylnmc/novum.vim/master/colors/novum.vim 2>/dev/null
 mkdir -p ~/.vim/colors
 mv novum.vim ~/.vim/colors/novum.vim
 
+echo "Setting .vimrc"
 echo "set number" >> ~/.vimrc
 echo "set tabstop=4" >> ~/.vimrc
 echo "set autoindent" >> ~/.vimrc
@@ -25,3 +30,6 @@ echo "syntax on" >> ~/.vimrc
 echo "set hlsearch" >> ~/.vimrc
 echo "colorscheme novum"
 echo "nnoremap \\ :noh<return>" >> ~/.vimrc
+
+sleep 2
+echo "Done!"
