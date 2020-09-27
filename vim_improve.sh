08b8d35420993a48d5e8bcbc81fad64fab3e5d20
Tanishq Rupaal <37408906+Tanq16@users.noreply.github.com>
2020-09-27 16:50:45 -0400

    Update script

--- vim_improve.sh
+++ vim_improve.sh
@@ -15,42 +15,20 @@ wget https://www.vim.org/scripts/download_script.php\?src_id\=21752 2>/dev/null
 mv 'download_script.php?src_id=21752' ~/.vim/supertab.vmb
 vim -c 'so %' -c 'q' ~/.vim/supertab.vmb
 
-echo "Installing novum colorscheme"
+echo "Installing nord colorscheme"
 wget https://raw.githubusercontent.com/dylnmc/novum.vim/master/colors/novum.vim 2>/dev/null
+git clone https://github.com/arcticicestudio/nord-vim.git 2>/dev/null
 mkdir -p ~/.vim/colors
-mv novum.vim ~/.vim/colors/novum.vim
+mv nord-vim/colors/nord.vim ~/.vim/colors/nord.vim
+mv nord-vim/autoload/lightline/colorscheme/nord.vim ~/.vim/pack/plugins/start/lightline/autoload/lightline/colorscheme/
+rm -rf nord-vim/
 
 echo "Installing nerd commenter"
 curl -fLo ~/.vim/plugin/NERD_Commenter.vim --create-dirs https://raw.githubusercontent.com/preservim/nerdcommenter/master/plugin/NERD_commenter.vim 2>/dev/null
 curl -fLo ~/.vim/doc/NERD_Commenter.txt --create-dirs https://raw.githubusercontent.com/preservim/nerdcommenter/master/doc/NERD_commenter.txt 2>/dev/null
 
-echo "Setting .vimrc"
-echo "set mouse=a" >> ~/.vimrc
-echo "set number" >> ~/.vimrc
-echo "set tabstop=4" >> ~/.vimrc
-echo "set autoindent" >> ~/.vimrc
-echo "set expandtab" >> ~/.vimrc
-echo "set laststatus=2" >> ~/.vimrc
-echo "syntax on" >> ~/.vimrc
-echo "set hlsearch" >> ~/.vimrc
-echo "colorscheme novum" >> ~/.vimrc
-
-echo "let g:lightline = {" >> ~/.vimrc
-echo "      \\ 'colorscheme': 'wombat'," >> ~/.vimrc
-echo "      \\}" >> ~/.vimrc
-echo "set noshowmode" >> ~/.vimrc
-echo "nnoremap \\\\ :noh<return>" >> ~/.vimrc
-echo "set rtp+=~/.fzf" >> ~/.vimrc
-echo "set cursorline" >> ~/.vimrc
-echo "nnoremap <C-m> :tabnext<return>" >> ~/.vimrc
-echo "nnoremap <C-n> :tabprev<return>" >> ~/.vimrc
-echo "nnoremap ff :FZF ~<return>" >> ~/.vimrc
-echo "nnoremap tt :tabnew<return>" >> ~/.vimrc
-echo "nnoremap nm :set invnumber<return>" >> ~/.vimrc
-
-echo "au BufReadPost * if line(\"'\\\"\") > 0 && line(\"'\\\"\") <= line(\"$\") | exe \"normal! g\`\\\"\" | endif" >> ~/.vimrc
-
-echo "filetype plugin on" >> ~/.vimrc
+mv ~/.vimrc ~/.vimrc.old
+wget https://raw.githubusercontent.com/Tanq16/cli-productivity-suite/master/.vimrc ~/.vimrc 2>/dev/null
 
 sleep 2
 echo "Done!"
