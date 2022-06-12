020617b0d83b4229a02e07ceba5ca678346be623
Tanishq Rupaal <37408906+Tanq16@users.noreply.github.com>
2022-06-12 12:53:20 -0500

    deprecate vimbonus for spacevim and fix install_zsh for mac

--- README.md
+++ README.md
@@ -1,52 +1,58 @@
 # Command Line Productivity Suite
 
-Use this script to easily install **zsh** and the custom shell experience of **Oh-My-Zsh**. This also installs `bat` as an alternative to `cat` with syntax highlighting. A bonus script is also present for quick settings of `vim`.
+Use this script to easily install **zsh** and the custom shell experience of **Oh-My-Zsh**. This also installs `bat` as an alternative to `cat` with syntax highlighting. As a bonus, SpaceVim is also installed for a flashy `vim` experience.
 
 ## Requirements
 
-The system should have `curl` installed. `vim` is required for the bonus script. If not, do it like so &rarr;
+The system should have `git`, `zsh`, `wget`, `curl` and `vim` installed. If not, do it like so &rarr;
+
 ```bash
-sudo apt install curl vim # Or `brew install curl vim` for MacOS
+sudo apt install git zsh wget curl vim # Or `brew install git zsh wget curl vim` for MacOS
 ```
 
 ## Installation
 
 The first step is to install `zsh`. Enter the password when prompted to make `zsh` default (or do it later as per necessity). Execute the following on your terminal &rarr;
+
 ```bash
-sudo apt install -y zsh wget git # Or `brew install wget git` for MacOS
 wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh 2>/dev/null
 sh install.sh
-rm install.sh
+rm install.sh # cleanup
 ```
 
 Once done, execute the following to install all other magic and enter the password whenever prompted.
+
 ```bash
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Tanq16/cli-productivity-suite/master/install_zsh.sh)"
 ```
+
 Or, clone the repo and run the `install_zsh.sh` script.
 
 Restart the shell (log out and back in on a linux machine or close the shell and start again on WSL). Execute `p10k configure` after logging in (preferably after installation of the font). This time, set the options according to the way the prompt must look. For any wierd looking prompts, see Post Installation steps below.
 
 ## Features
 
-This script installs the following -
+This script installs the following &rarr;
 1. Zsh
 2. OH-MY-ZSH custom shell with Powerlevel10k theme
 3. Fuzzy finder for awesome productivity
 3. Syntax highlighting for command line
 4. Auto-completion on command line
 5. Tmux with mouse and other quality of life improvements
+6. SpaceVim for a flashy vim experience
 
 ## Post Installation
 
 If the prompt looks funny or has an unintended look or the options for lock, debian logo, etc. were not visible, change the font of the terminal you use to a powerline-font or something that has ligatures and icon support. Highest recommendation from the author &rarr; FiraCode Nerd Font, Ubuntu Mono Powerline Derivative.
 
 To install powerline fonts from repositories on debian systems, use &rarr;
+
 ```bash
 sudo apt install fonts-powerline
 ```
 
 After this reconfigure the powerlevel10k using &rarr;
+
 ```bash
 p10k configure
 ```
@@ -59,21 +65,16 @@ Tmux is another addition in the scripts above. To make full use of the plugins i
 
 **Tmux on iTerm** can be very useful if certain settings are enabled to allow copy to Mac clipboard upon selection of content within the tmux terminal draw. Tmux plugins installed as a part of the scripts above enable mouse support.
 
-## Bonus: Basic Vim quick changes
+## Bonus: Bat Config
 
-To use the basic modern editor features in vim, use the given script to install the features as follows &rarr;
-```bash
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/Tanq16/cli-productivity-suite/master/vim_improve.sh)"
-```
-This installs the supertab (tab => autocomplete), auto-pair brackets, lightline plugin and sets numbering, expandtab, tab=4 spaces, autoindent and some shortcuts.
+Bat is an alternative of the `cat` command. It uses syntax highlighting and works similar to cat. Bat also enables pager by default i.e., the command is automatically piped to `less` when the output is larger than a threshold. This is disabled with the install script by using &rarr;
 
-## Bonus: Installation of Bat
-
-Bat is an alternative of the `cat` command. It uses syntax highlighting and works similar to cat. This is installed by default. Bat also enables pager by default i.e., the command is automatically piped to `less` when the output is larger than a threshold. This is disabled in the installion by using &rarr;
 ```bash
 export BAT_PAGER=''
 ```
+
 during the installation. You could re-enable the same by deleting the line in your `.zshrc`. Or you could also just paste the following &rarr;
+
 ```bash
 sed -i "s/export BAT_PAGER=''//" ~/.zshrc
 ```
--- install_zsh.sh
+++ install_zsh.sh
@@ -12,26 +12,33 @@ fi
 # chsh -s /usr/bin/zsh $USER
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k 2>/dev/null
-sed -i "s/robbyrussell/powerlevel10k\/powerlevel10k/" ~/.zshrc
+if [ $(uname -s) != "Darwin" ]
+then
+    sed -i "s/robbyrussell/powerlevel10k\/powerlevel10k/" ~/.zshrc
+else
+    sed -ie "s/robbyrussell/powerlevel10k\/powerlevel10k/" ~/.zshrc
+fi
 
 echo "Custom shell installed."
-echo "Installing Auto-suggestions"
 git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions 2>/dev/null
-echo "Installing Syntax highlighting"
 git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting 2>/dev/null
 
-sed -i "s/plugins=/plugins=(git zsh-autosuggestions zsh-syntax-highlighting) #/" ~/.zshrc
+if [ $(uname -s) != "Darwin" ]
+then
+    sed -i "s/plugins=/plugins=(git zsh-autosuggestions zsh-syntax-highlighting) #/" ~/.zshrc
+else
+    sed -ie "s/plugins=/plugins=(git zsh-autosuggestions zsh-syntax-highlighting) #/" ~/.zshrc
+fi
 
 echo "Installing Bat and fd-Find"
 if [ $(uname -s) != "Darwin" ]
 then
-    sudo apt install bat -y 1>/dev/null 2>/dev/null
-    sudo apt install fd-find -y 1>/dev/null 2>/dev/null
+    sudo apt install bat fd-find -y 1>/dev/null 2>/dev/null
 else
-    brew install vim bat fd 1>/dev/null 2>/dev/null
+    brew install bat fd 1>/dev/null 2>/dev/null
 fi
 
-echo "Installing Tmux - Upon first start, press Prefix then type :source-file ~/.tmux.conf, then press Prefix->I to install plugins properly."
+echo "Installing Tmux - On first start, press Prefix (ctrl+b) then press shift-i to install plugins properly."
 git clone --depth=1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm 2>/dev/null
 git clone --depth=1 https://github.com/jimeh/tmux-themepack.git ~/.tmux-themepack
 wget https://raw.githubusercontent.com/Tanq16/cli-productivity-suite/master/tmuxconf 2>/dev/null
@@ -44,19 +51,29 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf 2>/dev/null
 echo "Installing colored ls"
 if [ $(uname -s) != "Darwin" ]
 then
-    if [ $(uname -i) != "x86_64" ]
-        a=$(curl -s https://github.com/Peltoche/lsd/releases/latest | grep -oE "tag.+\"" | cut -d '/' -f2 | cut -d "\"" -f1) && \
-        wget "https://github.com/Peltoche/lsd/releases/download/$a/lsd_$a""_amd64.deb" && \
-        apt install -y "./lsd_$a""_amd64.deb" && rm "lsd_$a""_amd64.deb"
+    if [ $(uname -p) != "x86_64" ]
+        a=$(curl -L -s https://github.com/Peltoche/lsd/releases/latest | grep -oE "tag.+\"" | cut -d '/' -f2 | grep -vE "^[^0-9]" | cut -d "\"" -f1 | head -n 1) && \
+        wget "https://github.com/Peltoche/lsd/releases/download/$a/lsd_""$a""_amd64.deb" && \
+        apt install -y "./lsd_""$a""_amd64.deb" && \
+        rm "lsd_""$a""_amd64.deb"
     else
-        a=$(curl -s https://github.com/Peltoche/lsd/releases/latest | grep -oE "tag.+\"" | cut -d '/' -f2 | cut -d "\"" -f1) && \
-        wget "https://github.com/Peltoche/lsd/releases/download/$a/lsd_$a""_arm64.deb" && \
-        apt install -y "./lsd_$a""_arm64.deb" && rm "lsd_$a""_arm64.deb"
+        a=$(curl -L -s https://github.com/Peltoche/lsd/releases/latest | grep -oE "tag.+\"" | cut -d '/' -f2 | grep -vE "^[^0-9]" | cut -d "\"" -f1 | head -n 1) && \
+        wget "https://github.com/Peltoche/lsd/releases/download/$a/lsd_""$a""_arm64.deb" && \
+        apt install -y "./lsd_""$a""_arm64.deb" && \
+        rm "lsd_""$a""_arm64.deb"
 else
     brew install lsd 1>/dev/null 2>/dev/null
 fi
 
 wget https://raw.githubusercontent.com/Tanq16/cli-productivity-suite/master/add_to_rc 2>/dev/null
+if [ $(uname -s) != "Darwin" ]
+then
+    echo "finishing up"
+else
+    sed -ie "s/fdfind/fd/" ./add_to_rc
+    sed -ie "s/batcat/bat/" ./add_to_rc
+    sed -ie "s/alias bat=/\# alias bat=/" ./add_to_rc
+fi
 cat add_to_rc >> ~/.zshrc
 rm add_to_rc
 cp .zshrc temptemp
@@ -64,10 +81,9 @@ cat temptemp | grep -vE "^#" | grep -vE "^$" > .zshrc
 rm temptemp
 
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
+echo "If you don't see shapes properly, install powerline fonts (Read the README)"
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-echo "If you don't see shapes properly, install powerline fonts. The recommended font is given in the README of the repo for this script."
-echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-echo "Starting powerlevel config in 10 seconds. Ater the configuration, please close all shell instances and restart the shell for fuzzy search and zsh plugins to take effect......."
-sleep 10
+echo "Starting powerlevel config in 7 seconds."
+echo "Ater the configuration, close all shell instances and restart shell for all plugins to take effect"
+sleep 7
 exec zsh -l
