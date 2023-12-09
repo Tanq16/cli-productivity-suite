3b0bd53be27828a4650bdced8f70e374c7a6716b
Tanishq Rupaal <tanishq.rupaal@praetorian.com>
2023-12-08 23:04:08 -0600

    nit

--- debian-configure.sh
+++ debian-configure.sh
@@ -35,7 +35,7 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o 
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
 echo   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" |   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo nala update -y && sudo nala install -y \
+sudo nala update && sudo nala install \
 	docker-ce docker-ce-cli containerd.io \
 	docker-buildx-plugin docker-compose-plugin
 sudo groupadd docker
@@ -48,7 +48,7 @@ nano .config/user-dirs.dirs # change unwanted ones to $HOME only
 # restart the system
 
 # cli productivity suite
-sudo nala install -y \
+sudo nala install \
 	tar wget tree tmux jq ninja-build \
 	gettext make cmake unzip git file \
 	gcc bat fd-find zsh
